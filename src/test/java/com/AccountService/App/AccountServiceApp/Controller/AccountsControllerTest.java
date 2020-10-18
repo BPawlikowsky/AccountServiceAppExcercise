46090e0048600e428c21acc8b1da9fce68def48e
Bartosz <bartoszpawlikowsky@gmail.com>
2020-10-18 23:40:30 +0200

    All be it overtime, all major tests are done, this was for myself to finish, discount this commit

---
+++ src/test/java/com/AccountService/App/AccountServiceApp/Controller/AccountsControllerTest.java
@@ -0,0 +1,142 @@
+package com.AccountService.App.AccountServiceApp.Controller;
+
+import com.AccountService.App.AccountServiceApp.Models.Account;
+import com.AccountService.App.AccountServiceApp.Models.AccountsRepository;
+import com.AccountService.App.AccountServiceApp.Models.Requests.CreateAccountRequest;
+import com.AccountService.App.AccountServiceApp.Models.Requests.TransferMoneyRequest;
+import com.AccountService.App.AccountServiceApp.Service.AccountsService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Currency;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+public class AccountsControllerTest {
+
+    private List<Account> accountList;
+
+    @Mock
+    AccountsService accountsService;
+
+    @Mock
+    AccountsRepository accountsRepository;
+
+    @Mock
+    AccountsController accountsController;
+
+    @Test
+    public void createAccount() {
+        ResponseEntity<HttpStatus> expectedResponse = new ResponseEntity<>(HttpStatus.CREATED);
+        CreateAccountRequest request = new CreateAccountRequest(
+                "Test1",
+                Currency.getInstance("EUR"),
+                BigDecimal.valueOf(1000),
+                true
+        );
+
+        when(accountsController.createAccount(request)).thenReturn(expectedResponse);
+        ResponseEntity<HttpStatus> actualResponse = accountsController.createAccount(request);
+        assertEquals(expectedResponse, actualResponse);
+    }
+
+    @Before
+    public void createTestAccounts() {
+        Account fromTestAccount = new Account(
+                "fromTest",
+                Currency.getInstance("EUR"),
+                BigDecimal.valueOf(1000),
+                false
+        );
+        Account toTestAccount = new Account(
+                "toTest",
+                Currency.getInstance("EUR"),
+                BigDecimal.valueOf(1000),
+                false
+        );
+        accountsRepository.save(toTestAccount);
+        accountsRepository.save(fromTestAccount);
+    }
+
+    @Before
+    public void prepareAccountList() {
+        Account fromTestAccount = new Account(
+                "fromTest",
+                Currency.getInstance("EUR"),
+                BigDecimal.valueOf(1000),
+                false
+        );
+        Account toTestAccount = new Account(
+                "toTest",
+                Currency.getInstance("EUR"),
+                BigDecimal.valueOf(1000),
+                false
+        );
+        accountList = Arrays.asList(fromTestAccount, toTestAccount);
+    }
+
+    @Test
+    public void listAllAccounts() {
+        ResponseEntity<List<Account>> expectedResponse = new ResponseEntity<>(accountList, HttpStatus.FOUND);
+
+        when(accountsController.listAllAccounts()).thenReturn(expectedResponse);
+        ResponseEntity<List<Account>> actualResponse = accountsController.listAllAccounts();
+        assertEquals(expectedResponse, actualResponse);
+    }
+
+    @Test
+    public void findByName() {
+        ResponseEntity<List<Account>> expectedResponse = new ResponseEntity<>(List.of(accountList.get(0)), HttpStatus.FOUND);
+
+        when(accountsController.findByName("fromTest")).thenReturn(expectedResponse);
+        ResponseEntity<List<Account>> actualResponse = accountsController.findByName("fromTest");
+        assertEquals(expectedResponse, actualResponse);
+    }
+
+    @Test
+    public void findByCurrency() {
+        ResponseEntity<List<Account>> expectedResponse = new ResponseEntity<>(accountList, HttpStatus.FOUND);
+
+        when(accountsController.findByCurrency("EUR")).thenReturn(expectedResponse);
+        ResponseEntity<List<Account>> actualResponse = accountsController.findByCurrency("EUR");
+        assertEquals(expectedResponse, actualResponse);
+    }
+
+    @Test
+    public void findByTreasury() {
+        ResponseEntity<List<Account>> expectedResponse = new ResponseEntity<>(accountList, HttpStatus.FOUND);
+
+        when(accountsController.findByTreasury(false)).thenReturn(expectedResponse);
+        ResponseEntity<List<Account>> actualResponse = accountsController.findByTreasury(false);
+        assertEquals(expectedResponse, actualResponse);
+    }
+
+    @Test
+    public void transferMoney() {
+        TransferMoneyRequest request = new TransferMoneyRequest(
+                "fromTest",
+                "toTest",
+                BigDecimal.valueOf(100)
+        );
+        ResponseEntity<HttpStatus> expectedResponse = new ResponseEntity<>(HttpStatus.ACCEPTED);
+
+        when(accountsController.transferMoney(request)).thenReturn(expectedResponse);
+        ResponseEntity<HttpStatus> actualResponse = accountsController.transferMoney(request);
+        assertEquals(expectedResponse, actualResponse);
+    }
+}
--- src/test/java/com/AccountService/App/AccountServiceApp/Service/AccountsServiceTest.java
+++ src/test/java/com/AccountService/App/AccountServiceApp/Service/AccountsServiceTest.java
@@ -4,6 +4,8 @@ import com.AccountService.App.AccountServiceApp.Models.Account;
 import com.AccountService.App.AccountServiceApp.Models.AccountsRepository;
 import com.AccountService.App.AccountServiceApp.Models.Requests.CreateAccountRequest;
 import com.AccountService.App.AccountServiceApp.Models.Requests.TransferMoneyRequest;
+import org.assertj.core.util.Lists;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
@@ -12,11 +14,14 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import org.springframework.test.context.junit4.SpringRunner;
 
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.Currency;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
@@ -25,6 +30,8 @@ import static org.mockito.Mockito.when;
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
 public class AccountsServiceTest {
+
+    private List<Account> accountList;
 
     @Mock
     private AccountsRepository accountsRepository;
@@ -191,9 +198,100 @@ public class AccountsServiceTest {
         assertEquals(expectedResponse, actualResponse);
     }
 
-    @Test
-    @DisplayName("When findAll recieved and database as fields, return true")
-    public void findAll() {
+    @Before
+    public void prepareAccountList() {
+        Account fromTestAccount = new Account(
+                "fromTest",
+                Currency.getInstance("EUR"),
+                BigDecimal.valueOf(1000),
+                false
+        );
+        Account toTestAccount = new Account(
+                "toTest",
+                Currency.getInstance("EUR"),
+                BigDecimal.valueOf(1000),
+                false
+        );
+        accountList = Arrays.asList(fromTestAccount, toTestAccount);
+    }
 
+    @Test
+    @DisplayName("When findAll recieved and database as fields, non empty list")
+    public void findAll() {
+        List<Account> expectedResponse = accountList;
+        when(accountsService.getAllAccounts()).thenReturn(accountList);
+
+        List<Account> actualResponse = accountsService.getAllAccounts();
+        assertEquals(expectedResponse, actualResponse);
+    }
+
+    @Test
+    @DisplayName("When findAll recieved and database empty, return empty list")
+    public void findAll_NotFound() {
+        List<Account> expectedResponse = Lists.emptyList();
+        when(accountsService.getAllAccounts()).thenReturn(Lists.emptyList());
+
+        List<Account> actualResponse = accountsService.getAllAccounts();
+        assertEquals(expectedResponse, actualResponse);
+    }
+
+    @Test
+    @DisplayName("When findByName found, return Non empty List")
+    public void findByName_Found() {
+        List<Account> expectedResponse = List.of(accountList.get(0));
+        when(accountsService.findAccountByName("fromTest")).thenReturn(expectedResponse);
+
+        List<Account> actualResponse = accountsService.findAccountByName("fromTest");
+        assertEquals(expectedResponse, actualResponse);
+    }
+
+    @Test
+    @DisplayName("When findByName not found, return empty List")
+    public void findByName_NotFound() {
+        List<Account> expectedResponse = Lists.emptyList();
+        when(accountsService.findAccountByName("TestX")).thenReturn(expectedResponse);
+
+        List<Account> actualResponse = accountsService.findAccountByName("TestX");
+        assertEquals(expectedResponse, actualResponse);
+    }
+
+    @Test
+    @DisplayName("When findByCurrency found, return Non empty List")
+    public void findByCurrency_Found() {
+        List<Account> expectedResponse = accountList;
+        when(accountsService.findAccountByCurrency("EUR")).thenReturn(expectedResponse);
+
+        List<Account> actualResponse = accountsService.findAccountByCurrency("EUR");
+        assertEquals(expectedResponse, actualResponse);
+    }
+
+    @Test
+    @DisplayName("When findByCurrency found, return empty List")
+    public void findByCurrency_NotFound() {
+        List<Account> expectedResponse = Lists.emptyList();
+        when(accountsService.findAccountByCurrency("USD")).thenReturn(expectedResponse);
+
+        List<Account> actualResponse = accountsService.findAccountByCurrency("USD");
+        assertEquals(expectedResponse, actualResponse);
+    }
+
+    @Test
+    @DisplayName("When findByCurrency found, return Non empty List")
+    public void findByTreasury_Found() {
+        List<Account> expectedResponse = accountList;
+        when(accountsService.findAccountByTreasury(true)).thenReturn(expectedResponse);
+
+        List<Account> actualResponse = accountsService.findAccountByTreasury(true);
+        assertEquals(expectedResponse, actualResponse);
+    }
+
+    @Test
+    @DisplayName("When findByCurrency found, return empty List")
+    public void findByTreasury_NotFound() {
+        List<Account> expectedResponse = Lists.emptyList();
+        when(accountsService.findAccountByTreasury(false)).thenReturn(expectedResponse);
+
+        List<Account> actualResponse = accountsService.findAccountByTreasury(false);
+        assertEquals(expectedResponse, actualResponse);
     }
 }
