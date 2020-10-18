f2ca5af56d633d5426f24984616cdb713034feda
Bartosz <bartoszpawlikowsky@gmail.com>
2020-10-18 18:23:12 +0200

    Added bare bones generated starter SpringBoot project: Lombok, Spring Web, Spring Data JPA, H2 Database

---
+++ src/main/java/com/AccountService/App/AccountServiceApp/AccountServiceAppApplication.java
@@ -0,0 +1,13 @@
+package com.AccountService.App.AccountServiceApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountServiceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountServiceAppApplication.class, args);
+	}
+
+}
